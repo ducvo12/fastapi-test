67726bc40b3acf018096c7c87162912403332a5b
Duc Vo <duc.vo.hm@gmail.com>
2025-06-17 15:01:52 -0500

    Update fastapi.py

--- fastapi.py
+++ fastapi.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/api/greeting")
 def get_greeting(name: str = "FastAPI!"):
     return {"message": f"Hello from {name}"}
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from Render!"}
